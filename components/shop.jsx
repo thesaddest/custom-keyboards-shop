5f08675051c826033a166d4c2952c523e2eaab6e
thesaddest <youngscarlxrd@yandex.by>
2022-06-07 09:06:09 +0200

    adding grid layout to shop

--- components/shop.jsx
+++ components/shop.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Shop = () => {
     return (
-        <div className="flex justify-center items-center bg-[#f8fafc] z-20 h-[92vh]">
-            Shop
+        <div className="flex justify-center items-center bg-[#f8fafc] z-20 h-[92vh] w-full">
+            <div className="flex w-full h-full">
+                <div className="w-full grid grid-cols-2 grid-flow-col gap-3 md:grid-cols-3 lg:grid-cols-4">
+                    shop
+                </div>
+            </div>
         </div>
     );
 };
