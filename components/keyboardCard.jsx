37a4c8585aa87eee1dbea40e5c51a96e3114a676
thesaddest <youngscarlxrd@yandex.by>
2022-06-08 11:32:02 +0200

    data display

--- components/keyboardCard.jsx
+++ components/keyboardCard.jsx
@@ -1,45 +1,29 @@
 import React from "react";
 
-const KeyboardCard = () => {
-    const testKBs = [
-        {
-            title: "FL680",
-            description: "Small layout, easy to carry and no worries",
-            price: "$115.99",
-            url: "https://aliexpress.ru/item/1005002766739501.html?spm=a2g2w.productlist.0.0.2fb67c9eWblJbF&sku_id=12000028865809436",
-            img: "https://img.joomcdn.net/c6c178e175db9446805739fa9418593f7c533f7f_original.jpeg",
-        },
-    ];
-
+const KeyboardCard = ({ title, description, url, img, price }) => {
     return (
         <div className="pb-9 mb-0">
-            {testKBs.map((testKB, i) => (
-                <div key={i} className="pb-9 mb-0">
-                    <div className="min-h-[165px] mb-2 flex flex-col items-center justify-end">
-                        <a href="#">
-                            <img src={testKB.img} />
-                        </a>
-                    </div>
-                    <div className="flex flex-col items-center mt-2">
-                        <h3 className="text-2xl font-semibold mb-1">
-                            {testKB.title}
-                        </h3>
-                        <p className="text-lg font-normal leading-5 flex min-h-[50px] max-w-[160px] justify-center mt-2">
-                            {testKB.description}
-                        </p>
-                        <p className="text-sm font-normal mt-2">
-                            From {testKB.price}
-                        </p>
-                        <a
-                            href={testKB.url}
-                            className="bg-[#121212] text-white rounded-[980px] whitespace-nowrap text-center inline-block cursor-pointer
-                            text-[12px] font-normal min-w-[23px] px-3 py-1 mt-4"
-                        >
-                            Buy
-                        </a>
-                    </div>
+            <div className="pb-9 mb-0">
+                <div className="min-h-[165px] mb-2 flex flex-col items-center justify-center">
+                    <a href={url} target="_blank">
+                        <img src={img} />
+                    </a>
                 </div>
-            ))}
+                <div className="flex flex-col items-center mt-2">
+                    <h3 className="text-2xl font-semibold mb-1">{title}</h3>
+                    <p className="text-lg font-normal leading-5 flex min-h-[50px] max-w-[160px] justify-center mt-2">
+                        {description}
+                    </p>
+                    <p className="text-sm font-normal mt-2">From {price}</p>
+                    <a
+                        href={url}
+                        className="bg-[#121212] text-white rounded-[980px] whitespace-nowrap text-center inline-block cursor-pointer
+                            text-[12px] font-normal min-w-[23px] px-3 py-1 mt-4"
+                    >
+                        Buy
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
--- components/shop.jsx
+++ components/shop.jsx
@@ -2,17 +2,56 @@ import React from "react";
 import KeyboardCard from "./keyboardCard";
 
 const Shop = () => {
+    const data = [
+        {
+            title: "FL680",
+            description: "Small layout, easy to carry and no worries",
+            price: "$115.99",
+            url: "https://aliexpress.ru/item/1005002766739501.html?spm=a2g2w.productlist.0.0.2fb67c9eWblJbF&sku_id=12000028865809436",
+            img: "https://img.joomcdn.net/c6c178e175db9446805739fa9418593f7c533f7f_original.jpeg",
+        },
+        {
+            title: "FL Q75",
+            description: "High-gloss mirror shell, the appearance is lit",
+            price: "$145.99",
+            url: "https://a.aliexpress.com/_m0FHiqI",
+            img: "https://hanoicomputercdn.com/media/product/65246_ban_phim_co_khong_day_fl_esport_q75_blue_haze_3_mode_jellyfish_sw_kailh_box_usbc_bluetooth_rgb_0000_1.jpg",
+        },
+        {
+            title: "FL CMK87",
+            description: "Color scheme aluminium alloy hit",
+            price: "$155.99",
+            url: "https://a.aliexpress.com/_mtegtE6",
+            img: "https://product.hstatic.net/1000026716/product/61414_ban_phim_co_flesport_cmk87_ice_mint_coolmint_sw_kailh_box_0004_5_a5ce03d9b8634940b95f54346343d319.jpg",
+        },
+        {
+            title: "FL 980",
+            description: "98 keys to save the desktop space",
+            price: "$165.99",
+            url: "https://a.aliexpress.com/_m0CFvHC",
+            img: "https://cdn.shopify.com/s/files/1/0588/1108/9090/products/1_33c6769b-dd65-41a4-a471-a1ae88b18207_1024x1024@2x.jpg?v=1645008251",
+        },
+    ];
+
     return (
-        <section className="bg-white h-[122vh] w-full pt-16 pb-16 text-center">
+        <section className="bg-white h-fit w-full pt-16 pb-16 text-center">
             <div className="mx-auto w-[87.5%] ">
                 <h2 className="text-3xl text-[#121212] font-semibold">
                     Which Mech Keyboard is right for you?
                 </h2>
                 <div className="grid grid-cols-2 grid-rows-16 gap-x-2.5 max-w-[500px] mt-11">
-                    <KeyboardCard />
-                    <KeyboardCard />
-                    <KeyboardCard />
-                    <KeyboardCard />
+                    {data.map((keyboard, i) => {
+                        return (
+                            <KeyboardCard
+                                title={keyboard.title}
+                                description={keyboard.description}
+                                price={keyboard.price}
+                                url={keyboard.url}
+                                img={keyboard.img}
+                                key={i}
+                            />
+                        );
+                    })}
                 </div>
             </div>
         </section>
