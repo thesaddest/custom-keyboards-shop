857fdba35abdee650f28431da6046b427dfeea4a
thesaddest <youngscarlxrd@yandex.by>
2022-06-14 14:38:53 +0200

    add: parallax section

--- components/works.jsx
+++ components/works.jsx
@@ -10,7 +10,7 @@ import {
 import {WorkBackground, WorkContainer} from "./work";
 
 const Works = () => (
-    <TileWrapper numOfPages={3}>
+    <TileWrapper numOfPages={4}>
         <TileBackground>
             <WorkBackground/>
         </TileBackground>
@@ -67,6 +67,25 @@ const Works = () => (
                             <img
                                 src="https://product.hstatic.net/1000026716/product/61414_ban_phim_co_flesport_cmk87_ice_mint_coolmint_sw_kailh_box_0004_5_a5ce03d9b8634940b95f54346343d319.jpg"
                                 alt="FL CMK87"
+                            />
+                        </WorkRight>
+                    </WorkContainer>
+                )}
+            ></Tile>
+            <Tile
+                page={3}
+                renderContent={({progress}) => (
+                    <WorkContainer>
+                        <WorkLeft progress={progress}>
+                            <div>We built</div>
+                            <div className="text-4xl md:text-5xl font-semibold tracking-tight">
+                                FL 980.
+                            </div>
+                        </WorkLeft>
+                        <WorkRight progress={progress}>
+                            <img
+                                src="https://cdn.shopify.com/s/files/1/0588/1108/9090/products/1_33c6769b-dd65-41a4-a471-a1ae88b18207_1024x1024@2x.jpg?v=1645008251"
+                                alt="FL 980"
                             />
                         </WorkRight>
                     </WorkContainer>
